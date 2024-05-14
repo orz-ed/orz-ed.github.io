0fa74a8c58ca1b724b8335047661c5e53daaa887
orzed <130018406+orz-ed@users.noreply.github.com>
2024-05-14 14:13:46 +1000

    w

--- week8/script.js
+++ week8/script.js
@@ -1,2 +1,8 @@
-const.myVideo = document.querySelector("my-video");
-console.log(myVideo)
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+
+const playPauseBtn = document.querySelector("#playpausebtn");
+console.log(playPauseBtn);
+
+const plauPauseImg = document.querySelector("#playpauseimg");
+console.log(playPauseImg);
