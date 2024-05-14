03c86e41c1de06944f23f514ab4d0bfa850c2368
orzed <130018406+orz-ed@users.noreply.github.com>
2024-05-14 14:58:32 +1000

    a

--- week8/script.js
+++ week8/script.js
@@ -4,5 +4,50 @@ console.log(myVideo);
 const playPauseBtn = document.querySelector("#playpausebtn");
 console.log(playPauseBtn);
 
-const plauPauseImg = document.querySelector("#playpauseimg");
+const playPauseImg = document.querySelector("#playpauseimg");
 console.log(playPauseImg);
+
+playPauseBtn.addEventListener("click", toggleVideoPlayback);
+
+function toggleVideoPlayback() {
+  if (myVideo.paused || myVideo.ended) {
+    playPauseImg.src = "files/pausebtn.png";
+    myVideo.play();
+  } else {
+    playPauseImg.src = "files/playbtn.png";
+    myVideo.pause();
+  }
+}
+
+// -------------------------------
+
+const muteUnmutebtn = document.querySelector("#muteunmutebtn");
+console.log(muteUnmutebtn);
+
+const muteUnmuteImg = document.querySelector("#muteunmuteimg");
+console.log(muteUnmuteImg);
+
+muteUnmutebtn.addEventListener("click", toggleSound);
+
+function toggleSound() {
+  if (myVideo.muted) {
+    muteUnmuteImg.src = "files/unmute.png";
+    muteUnmutebtn.style.backgroundColor = "blue";
+    myVideo.muted = false;
+  } else {
+    muteUnmuteImg.src = "files/muted.png";
+    muteUnmutebtn.style.backgroundColor = "red";
+    myVideo.muted = true;
+  }
+}
+
+myVideo.addEventListener("timeupdate", updateProgressBar);
+
+const progressBarFill = document.querySelector("#progress-bar-fill");
+console.log(progressBarFill);
+
+function updateProgressBar() {
+  const progress = (myVideo.currentTime / myVideo.duration) * 100;
+  progressBarFill.style.width = progress + "%";
+  console.log(progress);
+}
