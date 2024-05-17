d5813c2732ab4896fb5fa3dfe808eeacb8143b42
orzed <130018406+orz-ed@users.noreply.github.com>
2024-05-18 00:14:21 +1000

    f

--- assignment2/script.js
+++ assignment2/script.js
@@ -21,6 +21,47 @@ function toggleAudioPlayback() {
 
 // ---------------------------------------------------------------
 
+let loop = false;
+
+const loopBtn = document.querySelector("#loopbtn");
+loopBtn.addEventListener("click", loopAudio);
+
+myAudio.addEventListener("ended", replay);
+
+function replay() {
+  console.log("loop is", loop);
+  if (loop) {
+    myAudio.currentTime = 0;
+    myAudio.play();
+  }
+}
+
+function loopAudio() {
+  if (loop) {
+    loop = false;
+    loopBtn.style.backgroundColor = "#d5cea3";
+  } else {
+    loop = true;
+    loopBtn.style.backgroundColor = "#7b775e";
+  }
+  console.log("loop is", loop);
+}
+
+// ---------------------------------------------------------------
+
+myAudio.addEventListener("timeupdate", updateProgressBar);
+
+const progressBarFill = document.querySelector("#progbarfill");
+console.log(progressBarFill);
+
+function updateProgressBar() {
+  const progress = (myAudio.currentTime / myAudio.duration) * 100;
+  progressBarFill.style.width = progress + "%";
+  console.log(progress);
+}
+
+// ---------------------------------------------------------------
+
 const muteUnmutebtn = document.querySelector("#muteunmutebtn");
 console.log(muteUnmutebtn);
 
@@ -38,3 +79,32 @@ function toggleSound() {
     myAudio.muted = true;
   }
 }
+
+// ---------------------------------------------------------------
+
+const decreaseVolBtn = document.querySelector("#voldownbtn");
+decreaseVolBtn.addEventListener("click", decreaseVolume);
+
+const increaseVolBtn = document.querySelector("#volupbtn");
+increaseVolBtn.addEventListener("click", increaseVolume);
+
+// Event listener to check current volume
+myAudio.addEventListener("volumechange", updateVolume);
+
+function updateVolume() {
+  const volume = myAudio.volume;
+  console.log("Volume changed:", volume);
+}
+
+function increaseVolume() {
+  if (myAudio.volume < 1.0) {
+    myAudio.volume += 0.1;
+  }
+}
+function decreaseVolume() {
+  if (myAudio.volume > 0.1) {
+    myAudio.volume -= 0.1;
+  }
+}
+
+// ---------------------------------------------------------------
