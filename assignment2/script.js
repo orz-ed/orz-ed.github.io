78ca7c77363e720009863a0cb581abcbfa2b785d
orzed <130018406+orz-ed@users.noreply.github.com>
2024-05-21 20:32:30 +1000

    m

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,4 +1,4 @@
-//establish playlist ---------------------------------------------------------------
+// establish playlist ---------------------------------------------------------------
 
 const audioList = [
   {
@@ -212,31 +212,32 @@ var elapsedPausedTime = 0;
 
 function startStopwatch() {
   if (!stopwatchInterval) {
-    startTime = new Date().getTime() - elapsedPausedTime; 
-    stopwatchInterval = setInterval(updateStopwatch, 1000); 
+    startTime = new Date().getTime() - elapsedPausedTime;
+    stopwatchInterval = setInterval(updateStopwatch, 1000);
   }
 }
 
 function stopStopwatch() {
-  clearInterval(stopwatchInterval); 
-  elapsedPausedTime = new Date().getTime() - startTime; 
-  stopwatchInterval = null; 
+  clearInterval(stopwatchInterval);
+  elapsedPausedTime = new Date().getTime() - startTime;
+  stopwatchInterval = null;
 }
 
 function resetStopwatch() {
-  stopStopwatch(); 
-  elapsedPausedTime = 0; 
-  document.getElementById("stopwatch").innerHTML = "00:00:00"; 
+  stopStopwatch();
+  elapsedPausedTime = 0;
+  document.getElementById("stopwatch").innerHTML = "00:00:00";
 }
 
 function updateStopwatch() {
-  var currentTime = new Date().getTime(); 
-  var elapsedTime = currentTime - startTime; 
-  var seconds = Math.floor(elapsedTime / 1000) % 60; 
-  var minutes = Math.floor(elapsedTime / 1000 / 60) % 60; 
+  var currentTime = new Date().getTime();
+  var elapsedTime = currentTime - startTime;
+  var seconds = Math.floor(elapsedTime / 1000) % 60;
+  var minutes = Math.floor(elapsedTime / 1000 / 60) % 60;
   var hours = Math.floor(elapsedTime / 1000 / 60 / 60);
-  var displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds); 
-  document.getElementById("stopwatch").innerHTML = displayTime; 
+  var displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+  document.getElementById("stopwatch").innerHTML = displayTime;
+}
 
 function pad(number) {
   // add a leading zero if the number is less than 10
