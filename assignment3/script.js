1b12eb8ebb7cd16c8b8a71c7e8b535c3ef74fa3b
orzed <130018406+orz-ed@users.noreply.github.com>
2024-06-18 00:35:10 +1000

    ad

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,23 +1,31 @@
-function ChangeCharacter(character)
-{
-switch(character)
-{
+// I followed this tutorial: https://www.youtube.com/watch?v=ISeyrczkzGY to learn
+// how to connect the buttons to changing the character's full body profile
+
+function ChangeCharacter(character) {
+  switch (character) {
     case "yuzu":
-        document.getElementById("fullimg").src = "img/yuzu.png";
-        break;
+      document.getElementById("fullimg").src = "img/yuzu.png";
+      document.getElementById("info").innerHTML = "difficulty ★★★☆☆";
+      break;
     case "wolf":
-        document.getElementById("fullimg").src = "img/wolf.png";
-        break;
+      document.getElementById("fullimg").src = "img/wolf.png";
+      document.getElementById("info").innerHTML = "difficulty ★★☆☆☆";
+      break;
     case "witch":
-        document.getElementById("fullimg").src = "img/witch.png";
-        break;
+      document.getElementById("fullimg").src = "img/witch.png";
+      document.getElementById("info").innerHTML = "difficulty ★★★★★";
+      break;
     case "red":
-        document.getElementById("fullimg").src = "img/red.png";
-        break;
+      document.getElementById("fullimg").src = "img/red.png";
+      document.getElementById("info").innerHTML = "difficulty ★★★★☆";
+      break;
     case "fox":
-        document.getElementById("fullimg").src = "img/fox.png";
-        break;
+      document.getElementById("fullimg").src = "img/fox.png";
+      document.getElementById("info").innerHTML = "difficulty ★☆☆☆☆";
+      break;
     case "boy":
-        document.getElementById("fullimg").src = "img/boy.png";
-        break;
+      document.getElementById("fullimg").src = "img/boy.png";
+      document.getElementById("info").innerHTML = "difficulty ★★★★★";
+      break;
+  }
 }
